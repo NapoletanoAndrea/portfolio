cec7cc19a4c247db71873e6ee911b8361c867a0e
Andrea Napoletano <and.nap110@gmail.com>
2025-11-15 16:57:20 +0100

    added svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), svgr(), tailwindcss()],
   server: {
     allowedHosts: [
       "612d-212-124-181-28.ngrok-free.app", // ← your ngrok URL
